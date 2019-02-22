922e34fe6c21109140e22e95fb2d22595b2420c5
tmurvv <tmurv@shaw.ca>
2019-02-22 09:00:32 -0700

    completed recaptcha v3 upgrade

--- configForRepo.php
+++ configForRepo.php
@@ -1,16 +1,15 @@
 <?php
 
     //email info for contact form
-    //$email = 'tmurv@shaw.ca'; //for testing
     $email = 'lmurv@hotmail.com';
     $siteEmailRecipient = "Ron";
 
     //reCaptcha info for contact form
     //ver 3
     $recaptchaKey = '6LeoRpMUAAAAAEGwl3c_kDlGgFl9t1LzOm4ECVe0';
-    $recaptchaSecretKey = '6LeoRpMUAAAAADkbw8MI_ihoZX4akfg6twRiSD6I';
+    $recaptchaSecretKey = 'REDACTED';
 
     //ver 2
     // $recaptchaKey = '6LcrP3sUAAAAACZXFfHU362MVFkMvhuTo83bRnNr';
-    // $recaptchaSecretKey = '6LcrP3sUAAAAAPTW-3NduUAYmmvWtK5h9-vDEguE';
+    // $recaptchaSecretKey = 'REDACTED';
 ?>
--- contact.php
+++ contact.php
@@ -3,61 +3,78 @@
     
     if (isset($_POST['submit'])) {
         $result = '';
-        $recaptchaArray = ['secret' => $recaptchaSecretKey,
-        'response' => $_POST['g-recaptcha-response']
-        ];
-
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($recaptchaArray));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = json_decode(curl_exec($curl));
-        curl_close($curl);
-      
-        if (isset($response->success) && !$response->success == true) {
-            $result = 'ReCaptcha validation failed.';
-        }
-
-        if (isset($result) && !$result == 'ReCaptcha validation failed.') {
-            $inputName = $_POST['name'];
-            $inputEmail = $_POST['email'];
-            $inputPhone = $_POST['phone'];
-            $inputMessage = $_POST['message'];
-    
-            $mail_body = '<html>
-            <body style="font-family: Arial, Helvetica, sans-serif;
-                                line-height:1.8em;">
-            <p>Hello '.$siteEmailRecipient.', <br> A message with the following information was sent via the contact form on the J.Dolan Stories website:</p>
-            <p>Name: '.$inputName.'<br>
-            Email: '.$inputEmail.'<br>
-            Phone: '.$inputPhone.'<br>
-            Message: '.$inputMessage.'<br>
-            <br>
-            Have a nice day!<br>
-            J.Dolan Stories
-            </p>
-            </body>
-            </html>';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['g-recaptcha-response'])) {
+                
+            // Build POST request:
+            $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+            $recaptchaSecretKey = "6LeoRpMUAAAAADkbw8MI_ihoZX4akfg6twRiSD6I";
+            $recaptcha_response = $_POST['g-recaptcha-response'];
         
-            $subject = "Message from jdolanstories.com contact form";
-            $headers = "From: jdolanstories.com" . "\r\n";
-            $headers .= "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            // Make and decode POST request: $recaptchaSecretKey from config.php file
+            $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptchaSecretKey . '&response=' . $recaptcha_response);
+            $recaptcha = json_decode($recaptcha);
+
+            // Take action based on the score returned:
+            if (isset($recaptcha->score) && $recaptcha->score >= 0.5) {
+                // Verified - send email
+                
+                $inputName = $_POST['name'];
+                $inputEmail = $_POST['email'];
+                $inputPhone = $_POST['phone'];
+                $inputMessage = $_POST['message'];
+        
+                $mail_body = '<html>
+                <body style="font-family: Arial, Helvetica, sans-serif;
+                                    line-height:1.8em;">
+                <p>Hello '.$siteEmailRecipient.', <br> A message with the following information was sent via the contact form on the J.Dolan Stories website:</p>
+                <p>Name: '.$inputName.'<br>
+                Email: '.$inputEmail.'<br>
+                Phone: '.$inputPhone.'<br>
+                Message: '.$inputMessage.'<br>
+                <br>
+                Have a nice day!<br>
+                J.Dolan Stories
+                </p>
+                </body>
+                </html>';
             
-            //Error Handling for PHPMailer
-            if(!mail($email, $subject, $mail_body, $headers)){
-                $result = "Email failed to send.";
-            }
-            else{
-                $result = "Email sent!";
-            }
-        }       
-    }
+                $subject = "Message from jdolanstories.com contact form";
+                $headers = "From: jdolanstories.com" . "\r\n";
+                $headers .= "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                
+                //Error Handling for PHPMailer
+                if(!mail($email, $subject, $mail_body, $headers)){
+                    $result = "Email failed to send.";
+                }
+                else{
+                    $result = "Email sent!";
+                }
+                       
+            } else {
+                // Not verified - show form error
+                $result = 'Form failed to send. Please call or message Ron at 503-240-7144.';
+            }      
+        }
+    }    
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google reCaptcha -->
+    <script src="https://www.google.com/recaptcha/api.js?render=6LeoRpMUAAAAAEGwl3c_kDlGgFl9t1LzOm4ECVe0"></script>
+    <script>
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6LeoRpMUAAAAAEGwl3c_kDlGgFl9t1LzOm4ECVe0', {action: 'homepage'}).then(function(token) {
+                // pass the token to the backend script for verification
+
+                // add token value to form for PHP verification
+                document.getElementById('g-recaptcha-response').value = token;
+            });
+        });
+    </script>
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -68,7 +85,6 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
 
     <title>J--Dolan-Stories-Contact</title>
-    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
     <div class="contact">
@@ -97,8 +113,11 @@
                             <input type="text" name='phone' placeholder='optional'><br>
                         </div>
                         <label for="message">Message:</label>
-                        <textarea rows="4" name="message"></textarea><br>
-                        <div class="g-recaptcha" data-sitekey="6LcrP3sUAAAAACZXFfHU362MVFkMvhuTo83bRnNr"></div>
+                        <textarea rows="6" name="message"></textarea><br>
+                        <!-- reCaptcha fields -->
+                        <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+                        <input type="hidden" name="action" value="validate_captcha">
+                        <!-- end reCaptcha fields -->
                         <button type='submit' name='submit'>Submit</button>
                     </div>
                 </form>
